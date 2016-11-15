fcfd7c06ac41e5468e161d8f4e3696bd100867d5
Joseph Taylor Parrish <jparrishau@gmail.com>
2016-11-15 14:26:07 -0600

    Removed content from home
    Removed content from home page until I decide what to do with it.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,27 +8,16 @@
     <div class="row">
         <div class="col-md-12">
             <div class="panel panel-default">
-                <div class="panel-heading bold-ln-height-2">Home
-                    <div class="btn-group pull-right">
-                        <a href="#addPostModal" class="btn btn-default" data-toggle="modal" data-target="addPostModal" id="addPostModalBtn" title="Add Post">
-                            <i class="fa fa-plus" aria-hidden="true"></i>
-                        </a>
-                    </div>
-                </div>
-               
+                <div class="panel-heading bold-ln-height-2">Home</div>
                 <div class="panel-body">
-                    <div class="row">
-                       
+                    <div class="row">       
                     </div>
                 </div>
             </div>
         </div>
     </div> 
 </div>
-    
-@include('gallery/detail_modal')
-@include('gallery/add_post_modal')
-<script src="{{ asset('js/gallery/modals.js') }}"></script>
+
 @endsection
 
 
