fc58ccb3d32ce5601af902132843445ab164b4b2
Joseph Taylor Parrish <jparrishau@gmail.com>
2016-11-14 19:51:33 -0600

    Added category, asking_price and condition to posts migration

--- database/migrations/2016_11_10_235031_create_posts_table.php
+++ database/migrations/2016_11_10_235031_create_posts_table.php
@@ -16,9 +16,11 @@ class CreatePostsTable extends Migration
         Schema::create('posts', function (Blueprint $table) {
             $table->increments('id');
             $table->string('user_id');
-            $table->int('category_id');
+            $table->int('category');
             $table->string('title');
             $table->string('description');
+            $table->double('asking_price');
+            $table->string('item_condition');
             $table->string('state');
             $table->string('city');
             $table->integer('zipcode');
