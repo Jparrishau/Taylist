ac2d8189fb99e9f19ff2e9faacc8de58746dcb50
Joseph Taylor Parrish <jparrishau@gmail.com>
2016-11-15 14:28:55 -0600

    Added VueJS and dynamic data to modal buttons
    Added VueJS

--- resources/views/gallery.blade.php
+++ resources/views/gallery.blade.php
@@ -8,11 +8,11 @@
 @section('content')
 <div class="container-fluid">
     <div class="row">
-        <div class="col-md-12">
+        <div class="col-xs-12">
             <div class="panel panel-default" style="height:90%;">
-                <div class="panel-heading bold-ln-height-2">Furniture
+                <div class="panel-heading bold-ln-height-2"> {{ucfirst(trans($category))}}
                     <div class="btn-group pull-right">
-                        <a href="#addPostModal" class="btn btn-default" data-toggle="modal" data-target="addPostModal" id="addPostModalBtn" title="Add Post">
+                        <a href="#addPostModal" id="addPostModalBtn" class="btn btn-default" data-toggle="modal" title="Add Post">
                             <i class="fa fa-plus" aria-hidden="true"></i>
                         </a>
                     </div>
@@ -23,11 +23,15 @@
                         @if(count($posts) > 0)
                             @foreach($posts as $post)
                                 <div class="gallery-row col-lg-3 col-md-4 col-sm-6">
-                                    <a href="#postDetailModal" id="postDetailModalBtn" data-toggle="modal" data-title="TEST" data-target="{{ $post['id'] }}">
-                                        <img class="gallery-img" src="{{ asset('img/gallery/furniture/' . $post->photos[0]->photo_path) }}" alt="Couch" style="width:100%; height:100%;">
+                                    <a href="#postDetailModal" id="postDetailModalBtn" data-toggle="modal" data-post="{{$post}}" >
+                                        <img class="gallery-img" src="{{ asset('img/gallery/' . $category . '/' . $post->photos[0]->photo_path) }}"
+                                        alt="Couch" style="width:100%; height:100%;">
                                     </a>
                                 </div>
                             @endforeach
+                        @else
+                            <!-- No posts exist. Give notice.-->
+                            <p>Nothing to see here. Move along.</p>
                         @endif
                     </div>
                 </div>
