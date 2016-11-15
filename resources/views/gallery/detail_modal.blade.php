b3673234ac70571f35c76a826082f77c4cbe4d09
Joseph Taylor Parrish <jparrishau@gmail.com>
2016-11-15 14:29:25 -0600

    Added dynamic data population via VueJS
    Added VueJS components to load data to modal.

--- resources/views/gallery/detail_modal.blade.php
+++ resources/views/gallery/detail_modal.blade.php
@@ -1,26 +1,34 @@
 <!-- The Modal -->
-<div id="postDetailModal" class="details-modal">
+<div id="postDetailModal" class="details-modal fade">
   <!-- Modal content -->
-  <div class="modal-content">
+  <div id="vueid" class="modal-content">
     <div id="carousel-example-generic" class="carousel slide" data-ride="carousel" data-interval="false">
     <!-- Indicators -->
     <ol class="carousel-indicators">
-      <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
-      <li data-target="#carousel-example-generic" data-slide-to="1"></li>
-      <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+      <template v-for="(photo, index) of data.photos">
+        <template v-if="index == 0">
+            <li data-target="#carousel-example-generic" v-bind:data-slide-to="index" class="active" ></li>
+        </template>
+        <template v-else>
+            <li data-target="#carousel-example-generic" v-bind:data-slide-to="index" class=""></li>
+        </template>
+      </template>
     </ol>
   
     <!-- Wrapper for slides -->
     <div class="carousel-inner" role="listbox">
-      <div class="item active">
-        <img src="{{ asset('img/gallery/furniture/couch.jpg') }}" alt="Couch" style="width:100%; height:auto;">
-      </div>
-      <div class="item">
-        <img src="{{ asset('img/gallery/furniture/couch_2.jpg') }}" alt="Couch" style="width:100%; height:auto;">
-      </div>
-      <div class="item">
-        <img src="{{ asset('img/gallery/furniture/1478983049.jpg') }}" alt="Couch" style="width:100%; height:auto;">
-      </div>
+        <template v-for="(photo, index) of data.photos">
+          <template v-if="index == 0">
+            <div class="item active">
+              <img v-bind:src="'/img/gallery/{{$category}}/' + photo.photo_path" v-bind:alt="photo.photo_path" style="width:100%; height:auto;">
+            </div>
+          </template>
+          <template v-else>
+            <div class="item">
+              <img v-bind:src="'/img/gallery/{{$category}}/' + photo.photo_path" v-bind:alt="photo.photo_path" style="width:100%; height:auto;">
+            </div>
+          </template>
+        </template>
     </div>
   
     <!-- Controls -->
@@ -45,25 +53,10 @@
       <div id="detail_menu" class="tab-pane fade in active">
         <h3>Item Details</h3>
         <hr style="border-bottom: 1px solid #ddd;" />
-        <p id="app"><b>Location:</b> @{{message}}</p>
-        <p><b>Asking Price:</b> $0.00</p>
-        <p><b>Condition:</b> Filler</p>
-        <p><b>Description:</b> Lorem ipsum dolor sit amet, consectetur adipiscing elit.z Integer nec odio. Praesent libero.
-        Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris.
-        Fusce nec tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla.
-        Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.
-        Curabitur sodales ligula in libero. Sed dignissim lacinia nunc. Curabitur tortor. Pellentesque nibh. Aenean quam.
-        In scelerisque sem at dolor. Maecenas mattis. Sed convallis tristique sem. Proin ut ligula vel nunc egestas porttitor.
-        Morbi lectus risus, iaculis vel, suscipit quis, luctus non, massa. Fusce ac turpis quis ligula lacinia aliquet.
-        Mauris ipsum. Nulla metus metus, ullamcorper vel, tincidunt sed, euismod in, nibh. Quisque volutpat condimentum velit.
-        Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nam nec ante.
-        Sed lacinia, urna non tincidunt mattis, tortor neque adipiscing diam, a cursus ipsum ante quis turpis.
-        Nulla facilisi. Ut fringilla. Suspendisse potenti. Nunc feugiat mi a tellus consequat imperdiet. Vestibulum sapien.
-        Proin quam. Etiam ultrices. Suspendisse in justo eu magna luctus suscipit. Sed lectus. Integer euismod lacus luctus magna.
-        Quisque cursus, metus vitae pharetra auctor, sem massa mattis sem, at interdum magna augue eget diam.
-        Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Morbi lacinia molestie dui.
-        Praesent blandit dolor. Sed non quam. In vel mi sit amet augue congue elementum. Morbi in ipsum sit amet pede facilisis laoreet.
-        Donec lacus nunc, viverra nec.</p>
+        <p id="app"><b>Location:</b> @{{data.city}}, @{{data.state}}</p>
+        <p><b>Asking Price:</b> $@{{data.asking_price}}</p>
+        <p><b>Condition:</b> @{{data.item_condition}}</p>
+        <p><b>Description:</b> @{{data.description}}</span></p>
       </div>
       <div id="question_menu" class="tab-pane fade">
         <h3>Questions</h3>
