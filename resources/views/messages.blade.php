d1760fa53a1d612b186155081c69913448b7ecc0
Joseph Taylor Parrish <jparrishau@gmail.com>
2016-11-15 18:11:43 -0600

    Added messages template for message page
    Need to add message page.

---
+++ resources/views/messages.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.app')
+
+@section('css')
+    <link href="{{ asset('css/gallery/modals.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/gallery/gallery.css') }}" rel="stylesheet">
+@endsection
+
+@section('content')
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-xs-12">
+    
+        </div>
+    </div> 
+</div>
+@endsection
+
+@section('js')
+@endsection
