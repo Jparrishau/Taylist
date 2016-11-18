02e1ff1b21b55a094777d08dc93d625d33fa9837
Joseph Taylor Parrish <jparrishau@gmail.com>
2016-11-17 21:48:47 -0600

    Added new get routes for message system

--- routes/web.php
+++ routes/web.php
@@ -31,6 +31,8 @@ Route::group(['prefix' => 'messages'], function () {
     Route::get('/', ['as' => 'messages', 'uses' => 'MessagesController@index']);
     Route::get('create', ['as' => 'messages.create', 'uses' => 'MessagesController@create']);
     Route::post('/', ['as' => 'messages.store', 'uses' => 'MessagesController@store']);
-    Route::get('{id}', ['as' => 'messages.show', 'uses' => 'MessagesController@show']);
+    Route::get('/getCurrentUserThreads', ['as' => 'messages.show', 'uses' => 'MessagesController@getThreadsByCurrentUser']);
+    Route::get('/getMessagesByThreadId/{id}', ['as' => 'messages.show', 'uses' => 'MessagesController@getMessagesByThreadId']);
+    Route::get('/getThreadById/{id}', ['as' => 'messages.show', 'uses' => 'MessagesController@getThreadById']);
     Route::put('{id}', ['as' => 'messages.update', 'uses' => 'MessagesController@update']);
 });
